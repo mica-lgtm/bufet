b2de852dae4ed88ef0697a734a54de8d9b79e91a
mica-lgtm <mica@zasdigital.com>
2026-05-13 16:29:17 -0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "placehold.co" },
